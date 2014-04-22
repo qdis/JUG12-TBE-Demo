b6e38b1c3f9c0803e3dc2f643da900c9de237756
timo <timo.bejan@dialogdata.de>
2014-04-22 09:45:13 +0300

    @Improved annotations :)

--- ThisWillRun/src/main/java/ro/dialogdata/jug/Application.java
+++ ThisWillRun/src/main/java/ro/dialogdata/jug/Application.java
@@ -2,16 +2,14 @@ package ro.dialogdata.jug;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @EnableAutoConfiguration
 public class Application {
 	
 	@RequestMapping("/")
-    @ResponseBody
 	public String HelloJug(){
 		return "Hello JUG!";
 	}
